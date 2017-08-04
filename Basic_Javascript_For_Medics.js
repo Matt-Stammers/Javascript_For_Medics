1c809b5d94c982a1206d06202391cd0ee6c3f4da
Matt Stammers <matthewstammers@googlemail.com>
2017-08-04 15:45:10 +0100

    Create Basic_Javascript_For_Medics.js

---
+++ Basic_Javascript_For_Medics.js
@@ -0,0 +1,63 @@
+// This is a JS comment, remember <!-- is for HTML and /* is for CSS
+// This guide is going to assume some very basic understanding & I recommend sololearn app or codeacademy to get started with JS
+
+alert("Welcome Clinical Developer!")
+
+// we have our first peice of Javascript code. If you enter this into the chrome browser console it will come up with an alert.
+// try this out in incognito mode if you are logged into gmail
+
+// Ok so JS Datatypes first:
+
+// JS doesn't have floats and int's, just numbers as above
+
+10
+10.4
+- 19.66
+
+// These are JS strings
+
+"10"
+"10.4"
+"Welcome Clinical Developer!"
+
+// JS Booleans are lower case
+
+true
+false
+
+// these are other datatypes in JS
+
+undefined
+null
+
+// basic operators
+// sum, subract, mult, divide, exponentiation & multiply all apply the same as per python
+// try them out in the consule in the developer environment of your chrome browser go to settings, more tools and dev tools to get this up
+// or you can use an alternative JS programming environment
+
+1+1
+1-1
+3*4
+2/7
+2 ** 2
+14%2
+
+// string concatenation
+
+"Django" + "is awesome"
+
+// length
+
+"Django".length // will return 6
+
+// \n will start a new line
+
+"Django /n is awesome"
+
+// \t will act as a tab in the same manner
+// \ will also escape special characters
+
+"Django quote: \"it's awesome\" said the clin_dev"
+
+// indexing
+
