60dced906906dcc91b9f9c69778928b94f30ea25
Matt Stammers <matthewstammers@googlemail.com>
2017-08-04 15:56:33 +0100

    Update Basic_Javascript_For_Medics.js

--- Basic_Javascript_For_Medics.js
+++ Basic_Javascript_For_Medics.js
@@ -59,5 +59,35 @@ null
 
 "Django quote: \"it's awesome\" said the clin_dev"
 
-// indexing
+// indexing is also 0 based indexing as per python, so to grab the letter g from the above
+
+"Django quote: \"it's awesome\" said the clin_dev"[4]
+
+// variable assignment
+// var varName = value; Note snake_casing is heavily discouraged so use camelCasing - see bwelow
+
+var bankBalance = "100";
+bankBalance // this will return 100
+
+var name = "Clin_Dev: "
+name // this will return "Clin_Dev: "
+
+alert(name + bankBalance)
+// this will bring up another alert which says "Clin_Dev: 100" They are very useful to check your work
+
+var blob // this will return undefined
+var blobby = null; // this will return null
+
+// to output to the console
+
+console.log("The Clin_Dev is Stuck in the Console") // This will print to the console
+
+// prompt
+
+prompt("Enter your age: ") // This will output to the browser with a text dialogue box which will return the entry to the console
+
+// to save it as a variable
+
+var age = prompt("Enter your age: ") 
+age // this will then call the prompt and return the value to the console
 
